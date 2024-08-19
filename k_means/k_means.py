1b06bd54db818f88c6836f7f8a4c1d2ba6f80e11
Haricharan Reddy <bharicharanreddy@gmail.com>
2024-08-19 14:42:06 +0530

    commit

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -1,60 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-
 from sklearn.cluster import KMeans
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[2]:
-
-
 df=pd.read_csv("https://raw.githubusercontent.com/codebasics/py/master/ML/13_kmeans/income.csv")
 
-
-# In[3]:
-
-
 df.head()
-
-
-# In[4]:
-
 
 plt.scatter(df.Age,df['Income($)'])
 plt.xlabel('Age')
 plt.ylabel('Income($)')
 
-
-# In[5]:
-
-
 km = KMeans(n_clusters=3)
 y_predicted = km.fit_predict(df[['Age','Income($)']])
 y_predicted
 
-
-# In[6]:
-
-
 df['cluster']=y_predicted
 df.head()
 
-
-# In[7]:
-
-
 km.cluster_centers_
-
-
-# In[8]:
-
 
 df1 = df[df.cluster==0]
 df2 = df[df.cluster==1]
@@ -67,10 +32,6 @@ plt.xlabel('Age')
 plt.ylabel('Income ($)')
 plt.legend()
 
-
-# In[9]:
-
-
 scaler = MinMaxScaler()
 
 scaler.fit(df[['Income($)']])
@@ -79,42 +40,18 @@ df['Income($)'] = scaler.transform(df[['Income($)']])
 scaler.fit(df[['Age']])
 df['Age'] = scaler.transform(df[['Age']])
 
-
-# In[10]:
-
-
 df.head()
 
-
-# In[11]:
-
-
 plt.scatter(df.Age,df['Income($)'])
-
-
-# In[12]:
-
 
 km = KMeans(n_clusters=3)
 y_predicted = km.fit_predict(df[['Age','Income($)']])
 y_predicted
 
-
-# In[13]:
-
-
 df['cluster']=y_predicted
 df.head()
 
-
-# In[14]:
-
-
 km.cluster_centers_
-
-
-# In[15]:
-
 
 df1 = df[df.cluster==0]
 df2 = df[df.cluster==1]
@@ -125,10 +62,6 @@ plt.scatter(df3.Age,df3['Income($)'],color='black')
 plt.scatter(km.cluster_centers_[:,0],km.cluster_centers_[:,1],color='purple',marker='*',label='centroid')
 plt.legend()
 
-
-# In[16]:
-
-
 sse = []
 k_rng = range(1,10)
 for k in k_rng:
@@ -136,17 +69,7 @@ for k in k_rng:
     km.fit(df[['Age','Income($)']])
     sse.append(km.inertia_)
 
-
-# In[17]:
-
-
 plt.xlabel('K')
 plt.ylabel('Sum of squared error')
 plt.plot(k_rng,sse)
-
-
-
-
-
-
 
--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,110 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
-
-# In[3]:
-
-
 df=pd.read_csv("https://raw.githubusercontent.com/charanbhc/py/master/ML/1_linear_reg/homeprices.csv")
 
-
-# In[4]:
-
-
 get_ipython().run_line_magic('matplotlib', 'inline')
+
 plt.xlabel('area')
 plt.ylabel('price')
 plt.scatter(df.area,df.price,color='red',marker='+')
 
-
-# In[5]:
-
-
 new_df = df.drop('price',axis='columns')
 new_df
-
-
-# In[6]:
-
 
 price = df.price
 price
 
-
-# In[7]:
-
-
 reg = linear_model.LinearRegression()
 reg.fit(new_df,price)
 
-
-# In[8]:
-
-
 reg.predict([[3300]])
-
-
-# In[9]:
-
 
 reg.coef_
 
-
-# In[10]:
-
-
 reg.intercept_
-
-
-# In[11]:
-
 
 3300*135.78767123 + 180616.43835616432
 
-
-# In[12]:
-
-
 reg.predict([[5000]])
-
-
-# In[14]:
-
 
 area_df = pd.read_csv("https://raw.githubusercontent.com/charanbhc/py/master/ML/1_linear_reg/areas.csv")
 area_df.head(3)
 
-
-# In[15]:
-
-
 p = reg.predict(area_df)
 p
-
-
-# In[16]:
-
 
 area_df['prices']=p
 area_df
 
-
-# In[17]:
-
-
 area_df.to_csv("https://raw.githubusercontent.com/charanbhc/py/master/ML/1_linear_reg/prediction.csv")
-
-
-# In[ ]:
-
-
-
-
